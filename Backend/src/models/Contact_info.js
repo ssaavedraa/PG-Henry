f58fff8a1bf4823a4d2fdff847d461ed8ce70c60
brianweidl <90735422+brianweidl@users.noreply.github.com>
2022-03-01 11:45:02 -0300

    Add files via upload

---
+++ Backend/src/models/Contact_info.js
@@ -0,0 +1,34 @@
+const { DataTypes } = require('sequelize')
+
+module.exports = (sequelize) => {
+	sequelize.define('contact_info', {
+		telephone: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		postCode: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		city: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		street: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		streetNumber: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		floor: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		unit: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+	})
+}
