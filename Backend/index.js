4f07af6f502fb3cc522613bd4e163a934a473b3b
unknown <nunezsantiago@gmail.com>
2022-03-01 10:46:29 -0300

    setUp server/db

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,14 @@
-console.log("Server started");
+const server = require("./src/app.js");
+const { conn } = require("./src/db.js");
+
+// Syncing all the models at once.
+conn
+  .sync({ force: true })
+  .then(() => {
+    server.listen(3001, () => {
+      console.log("%s listening at 3001"); // eslint-disable-line no-console
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
