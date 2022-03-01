3d03d093e7879f6a750bbb92bdcd0c98faea2d58
unknown <nunezsantiago@gmail.com>
2022-03-01 18:48:42 -0300

    feat:getAll controller

--- Backend/src/controllers/painting/getAll.js
+++ Backend/src/controllers/painting/getAll.js
@@ -1,8 +1,32 @@
-const { Painting } = require("../../db");
+const { Painting, Artist, Technique } = require("../../db");
 
-const getAll = (req, res) => {
-  console.log(Painting);
-  res.send("aca te estoy mandando todo");
+const getAll = async (req, res) => {
+  const paintings = await Painting.findAll({
+    where: { isAvailable: true },
+    attributes: [
+      "id",
+      "title",
+      "description",
+      "orientation",
+      "height",
+      "width",
+      "price",
+    ],
+    include: [
+      {
+        model: Technique,
+        through: {
+          attributes: [],
+        },
+        attributes: ["id", "name", "description"],
+      },
+      {
+        model: Artist,
+        attributes: ["id", "name", "biography", "photo", "email", "score"],
+      },
+    ],
+  });
+  res.json(paintings);
 };
 
 module.exports = getAll;
