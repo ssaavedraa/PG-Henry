e6d56e5e5e3a20d8690019f740c06db3ab12da56
Facundo-Romano <facuromano121299@gmail.com>
2022-03-01 11:38:07 -0300

    models

---
+++ Backend/src/models/User.js
@@ -0,0 +1,41 @@
+const { DataTypes } = require('sequelize');
+
+
+
+module.exports = (sequelize) => {
+  sequelize.define('user', {
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      lastName: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isEmail: true
+        }
+      },
+      profilePicture: {
+        type: DataTypes.STRING,
+        validate: {
+          isUrl: true
+        }
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      isBanned: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+      },
+      role: {
+        type: DataTypes.ENUM("guest", "admin", "user"),
+        allowNull: false
+      }
+  });
+};
