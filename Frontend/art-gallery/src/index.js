b9c12cce1d83ec2ee59c5a00c7504a5c5f58ee8b
SantiagoJSdev <santiagoreyesmarin1986@gmail.com>
2022-03-02 12:15:46 -0500

    commit

--- Frontend/art-gallery/src/index.js
+++ Frontend/art-gallery/src/index.js
@@ -4,14 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { Provider } from "react-redux";
-import {store} from './redux/store/store'
-import { DetailOfArt } from './components/obrasDetail/DetailOfArt';
+
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <React.StrictMode>
+  <App />
+</React.StrictMode>,
   document.getElementById('root')
 );
 
