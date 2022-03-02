05c5f8294a4339094280559389c41e7a037b4fb5
brianweidl <brianweidl@hotmail.com>
2022-03-02 10:42:49 -0300

    add semicolons

--- Backend/src/db.js
+++ Backend/src/db.js
@@ -1,8 +1,8 @@
-require('dotenv').config()
-const { Sequelize, Op } = require('sequelize')
-const fs = require('fs')
-const path = require('path')
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env
+require("dotenv").config();
+const { Sequelize, Op } = require("sequelize");
+const fs = require("fs");
+const path = require("path");
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
 const sequelize = new Sequelize(
 	`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
@@ -10,30 +10,30 @@ const sequelize = new Sequelize(
 		logging: false, // set to console.log to see the raw SQL queries
 		native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 	}
-)
-const basename = path.basename(__filename)
+);
+const basename = path.basename(__filename);
 
-const modelDefiners = []
+const modelDefiners = [];
 
 // Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
-fs.readdirSync(path.join(__dirname, '/models'))
+fs.readdirSync(path.join(__dirname, "/models"))
 	.filter(
 		(file) =>
-			file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
+			file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
 	)
 	.forEach((file) => {
-		modelDefiners.push(require(path.join(__dirname, '/models', file)))
-	})
+		modelDefiners.push(require(path.join(__dirname, "/models", file)));
+	});
 
 // Injectamos la conexion (sequelize) a todos los modelos
-modelDefiners.forEach((model) => model(sequelize))
+modelDefiners.forEach((model) => model(sequelize));
 // Capitalizamos los nombres de los modelos ie: product => Product
-let entries = Object.entries(sequelize.models)
+let entries = Object.entries(sequelize.models);
 let capsEntries = entries.map((entry) => [
 	entry[0][0].toUpperCase() + entry[0].slice(1),
 	entry[1],
-])
-sequelize.models = Object.fromEntries(capsEntries)
+]);
+sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
@@ -47,30 +47,30 @@ const {
 	Review,
 	Technique,
 	User,
-} = sequelize.models
+} = sequelize.models;
 
 // Aca vendrian las relaciones
 
-Painting.belongsTo(Artist)
-Artist.hasMany(Painting)
-Painting.hasMany(Review)
-Review.belongsTo(Painting)
-Painting.belongsToMany(Technique, { through: 'painting_technique' })
-Technique.belongsToMany(Painting, { through: 'painting_technique' })
-Painting.hasMany(Photo)
-Photo.belongsTo(Painting)
-Painting.belongsToMany(Purchase, { through: 'painting_purchases' })
-Purchase.belongsToMany(Painting, { through: 'painting_purchases' })
-Purchase.belongsTo(Contact_info)
-Purchase.belongsTo(User)
-User.hasMany(Purchase)
-Review.belongsTo(Artist)
-Artist.hasMany(Review)
-Review.belongsTo(User)
-User.hasMany(Review)
+Painting.belongsTo(Artist);
+Artist.hasMany(Painting);
+Painting.hasMany(Review);
+Review.belongsTo(Painting);
+Painting.belongsToMany(Technique, { through: "painting_technique" });
+Technique.belongsToMany(Painting, { through: "painting_technique" });
+Painting.hasMany(Photo);
+Photo.belongsTo(Painting);
+Painting.belongsToMany(Purchase, { through: "painting_purchases" });
+Purchase.belongsToMany(Painting, { through: "painting_purchases" });
+Purchase.belongsTo(Contact_info);
+Purchase.belongsTo(User);
+User.hasMany(Purchase);
+Review.belongsTo(Artist);
+Artist.hasMany(Review);
+Review.belongsTo(User);
+User.hasMany(Review);
 
 module.exports = {
 	...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
 	conn: sequelize, // para importart la conexión { conn } = require('./db.js');
 	Op,
-}
+};
