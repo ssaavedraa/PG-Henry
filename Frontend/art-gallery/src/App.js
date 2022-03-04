e7e0f8a12cbbf27da07031a77cdaf3bd719edcc1
Yessica L. Rodriguez Muñoz <89185291+yessicarm@users.noreply.github.com>
2022-03-04 09:07:37 -0700

    Update App.js

--- Frontend/art-gallery/src/App.js
+++ Frontend/art-gallery/src/App.js
@@ -1,16 +1,17 @@
-import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import NavBar from './components/navbar/navbar.component';
-import DetailArtist from './components/DetailArtist/DetailArtist';
-import Artists from './components/Artists/Artists.component';
-import MyProfile from './components/AdminPanel/MyProfile/MyProfile';
-import Login from './components/Login/Login.component';
-import Home from './components/Home/Home';
-import AddArtists from './components/AdminPanel/AddArtists/AddArtists';
-import AddItems from './components/AdminPanel/AddItems/AddItems';
-import UnderConstruction from './components/UnderConstruction/UnderConstruction';
-import Footer from './components/Footer/Footer';
-import ContactUs from './components/ContactUs/ContactUs';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NavBar from "./components/navbar/navbar.component";
+import DetailArtist from "./components/DetailArtist/DetailArtist";
+import Artists from "./components/Artists/Artists.component";
+import MyProfile from "./components/AdminPanel/MyProfile/MyProfile";
+import Login from "./components/Login/Login.component";
+import AddArtists from "./components/AdminPanel/AddArtists/AddArtists";
+import AddItems from "./components/AdminPanel/AddItems/AddItems";
+import UnderConstruction from "./components/UnderConstruction/UnderConstruction";
+import Footer from "./components/Footer/Footer";
+import { DetailOfArt } from "./components/obrasDetail/DetailOfArt";
+import Home from "./components/Home/Home.jsx";
+import ContactUs from "./components/ContactUs/ContactUs";
 import Gallery from "./components/Gallery/Gallery";
 
 function App() {
@@ -19,15 +20,16 @@ function App() {
       <div className="App">
         <NavBar />
         <Routes>
-          <Route exact path='/' element={<Home />}/>
-          <Route exact path='/admin/addartist' element={<AddArtists/>}/>
-          <Route exact path='/admin' element={<MyProfile/>}/>
-          <Route exact path='/admin/additems' element={<AddItems/>}/>
-          <Route exact path='/artists' element={<Artists/>}/>
-          <Route exact path='/artists/:id' element={<DetailArtist/>}/>
-          <Route exact path='/login' element={<Login/>} />
-          <Route exact path='/under' element={<UnderConstruction/>}/>
-          <Route exact path='/contactus' element={<ContactUs/>}/>
+          <Route exact path="/artists" element={<Artists />} />
+          <Route exact path="/artists/:id" element={<DetailArtist />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/detailObra/:id" element={<DetailOfArt />} />
+          <Route exact path="/admin/addartist" element={<AddArtists />} />
+          <Route exact path="/admin" element={<MyProfile />} />
+          <Route exact path="/admin/additems" element={<AddItems />} />
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/under" element={<UnderConstruction />} />
+          <Route exact path="/contactus" element={<ContactUs />} />
           <Route exact path="/gallery" element={<Gallery />} />
         </Routes>
         <Footer />
