035d0dff79eaf96e77493b61c952eb904ae90fa3
jorgyrod <jorgsrodriguez@gmail.com>
2022-03-04 04:44:15 -0500

    Se modifican los estilos de los links y se agrega parte del enrutamiento

--- Frontend/art-gallery/src/containers/CardsPaints/CardsPaints.jsx
+++ Frontend/art-gallery/src/containers/CardsPaints/CardsPaints.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import CardPaint from "../../components/CardPaint/CardPaint";
+import {NavLink} from 'react-router-dom';
 import "./CardsPaints.css";
 
 function CardsPaints({ paintings }) {
@@ -7,16 +8,18 @@ function CardsPaints({ paintings }) {
     <div className="containerCards">
       {paintings &&
         paintings.map((paint) => (
-          <CardPaint
-            key={paint.id}
-            image={paint}
-            title={paint.title}
-            artist={paint.artist}
-            height={paint.height}
-            width={paint.width}
-            techniques={paint.techniques}
-            price={paint.price}
-          />
+          <NavLink to="/gallery" className="links">
+            <CardPaint
+              key={paint.id}
+              image={paint}
+              title={paint.title}
+              artist={paint.artist}
+              height={paint.height}
+              width={paint.width}
+              techniques={paint.techniques}
+              price={paint.price}
+            />
+          </NavLink>
         ))}
     </div>
   );
