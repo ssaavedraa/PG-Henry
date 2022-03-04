fcf483bee82db6f9e4d9b53213d63775646bfbe3
jorgyrod <jorgsrodriguez@gmail.com>
2022-03-04 03:07:28 -0500

    Se agrega el componente Paginacion funcional

--- Frontend/art-gallery/src/components/Gallery/Gallery.jsx
+++ Frontend/art-gallery/src/components/Gallery/Gallery.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getPaintings } from "../../redux/actions/actions";
 
 import Filters from "../Filters/Filters";
 import CardsPaints from "../../containers/CardsPaints/CardsPaints";
+import Pagination from "../Pagination/Pagination";
 
 import "./Gallery.css";
 
@@ -15,11 +16,35 @@ function Gallery() {
     dispatch(getPaintings());
   }, [dispatch]);
 
-  console.log(paintings);
+  //Pagination
+  const [page, setPage] = useState({
+    actualPage: 1,
+    paintsPerPage: 9,
+  });
+
+  const lastPaint = page.actualPage * page.paintsPerPage;
+  const firstPaint = lastPaint - page.paintsPerPage;
+  const actualPaints = paintings.slice(firstPaint, lastPaint);
+
+  const paginate = (pageNumber) =>
+    setPage({
+      ...page,
+      actualPage: pageNumber,
+    });
+  //----------------------
+
   return (
     <div className="gallery-container">
       <Filters />
-      <CardsPaints paintings={paintings}/>
+      <div className="cards-container">
+        <CardsPaints paintings={actualPaints} />
+        <Pagination
+          paintsPerPage={page.paintsPerPage}
+          allPaintings={paintings.length}
+          Paginate={paginate}
+          actualPage={page.actualPage}
+        />
+      </div>
     </div>
   );
 }
