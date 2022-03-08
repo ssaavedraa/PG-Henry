c8f0a4fd9a4957ca14994cfb6bb70335ad139d69
unknown <nunezsantiago@gmail.com>
2022-03-08 15:34:20 -0300

    feat: useAuth hooks + user routes fixes

--- Backend/src/controllers/auth/login.js
+++ Backend/src/controllers/auth/login.js
@@ -3,35 +3,39 @@ const { compareSync } = require("bcrypt");
 const createJWT = require("./utils/createJWT");
 
 const login = async (req, res) => {
-	try {
-		const user = await User.findOne({ where: { email: req.body.email } });
-		if (!user)
-			return res
-				.status(404)
-				.json({ status: "error", message: "Incorrect user or password" });
+  try {
+    const user = await User.findOne({ where: { email: req.body.email } });
+    if (!user)
+      return res.json({
+        status: "error",
+        message: "Incorrect user or password",
+      });
 
-		if (user.isBanned)
-			return res.json({ status: "error", message: "User is banned" });
-		if (!compareSync(req.body.password, user.password)) {
-			return res
-				.status(401)
-				.json({ status: "error", message: "Incorrect user or password" });
-		}
-		const token = createJWT(user.id);
+    if (user.isBanned)
+      return res.json({ status: "error", message: "User is banned" });
+    if (!compareSync(req.body.password, user.password)) {
+      return res.json({
+        status: "error",
+        message: "Incorrect user or password",
+      });
+    }
+    const token = createJWT(user.id);
 
-		const sendUserInfo = {
-			id: user.id,
-			name: user.firstName,
-			email: user.email,
-			role: user.role,
-		};
+    const sendUserInfo = {
+      id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      role: user.role,
+    };
 
-		return res.json({
-			user: sendUserInfo,
-			token,
-		});
-	} catch (e) {
-		res.status(400).send(e);
-	}
+    return res.json({
+      status: "ok",
+      user: sendUserInfo,
+      token,
+    });
+  } catch (e) {
+    res.status(400).send(e);
+  }
 };
 module.exports = login;
