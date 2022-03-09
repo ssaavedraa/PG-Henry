e74262a486970f0a9483d3169fe3c58651a78f6b
unknown <nunezsantiago@gmail.com>
2022-03-09 11:24:18 -0300

    feat: added default user and admin to .env

--- Backend/mockData/loadMockData.js
+++ Backend/mockData/loadMockData.js
@@ -5,25 +5,47 @@ const reviews = require("./data/reviews.json");
 const users = require("./data/users.json");
 const purchases = require("./data/purchases.json");
 const { Artist, Technique, Review, User, Purchase } = require("../src/db");
+const { hashSync } = require("bcrypt");
+
 const createPainting = require("../src/controllers/painting/utils/createPainting");
+const {
+  DEFAULT_USER_EMAIL,
+  DEFAULT_USER_PASSWORD,
+  DEFAULT_ADMIN_EMAIL,
+  DEFAULT_ADMIN_PASSWORD,
+} = process.env;
 
 const loadMockData = async () => {
-	await Technique.bulkCreate(techniques);
-	await Artist.bulkCreate(artists);
-	await User.bulkCreate(users);
-	for (let p of paintings) {
-		await createPainting(p);
-	}
+  await Technique.bulkCreate(techniques);
+  await Artist.bulkCreate(artists);
+  await User.bulkCreate(users);
+  await User.create({
+    firstName: "user",
+    lastName: "default",
+    email: DEFAULT_USER_EMAIL,
+    password: hashSync(DEFAULT_USER_PASSWORD, 10),
+    role: "user",
+  });
+  await User.create({
+    firstName: "admin",
+    lastName: "default",
+    email: DEFAULT_ADMIN_EMAIL,
+    password: hashSync(DEFAULT_ADMIN_PASSWORD, 10),
+    role: "user",
+  });
+  for (let p of paintings) {
+    await createPainting(p);
+  }
 
-	for (let i = 0; i < purchases.length; i++) {
-		const createdPurchase = await Purchase.create(purchases[i]);
-		await createdPurchase.addPaintings(purchases[i].paintingId);
-	}
-	/* for (let i = 0; i < reviews.length; i++) {
+  for (let i = 0; i < purchases.length; i++) {
+    const createdPurchase = await Purchase.create(purchases[i]);
+    await createdPurchase.addPaintings(purchases[i].paintingId);
+  }
+  /* for (let i = 0; i < reviews.length; i++) {
 		const createdReview = await Review.create(reviews[i]);
 		await createdReview.addUser(reviews[i].userId);
 	} */
-	await Review.bulkCreate(reviews);
+  await Review.bulkCreate(reviews);
 };
 
 module.exports = loadMockData;
