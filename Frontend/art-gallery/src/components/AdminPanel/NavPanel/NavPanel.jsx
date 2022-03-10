c549386da325b5bf441b0f05d5341465f4630d32
unknown <nunezsantiago@gmail.com>
2022-03-10 13:31:17 -0300

    logout quickfix

--- Frontend/art-gallery/src/components/AdminPanel/NavPanel/NavPanel.jsx
+++ Frontend/art-gallery/src/components/AdminPanel/NavPanel/NavPanel.jsx
@@ -3,53 +3,49 @@ import "./NavPanel.css";
 import img from "../../../assets/img/profile.png";
 import { Link } from "react-router-dom";
 import { FiLogOut } from "react-icons/fi";
-import { useDispatch, useSelector } from "react-redux";
-import { setLogout } from "../../../redux/actions/actions";
+import { useSelector } from "react-redux";
 import { BsPersonBadge } from "react-icons/bs";
 import { FaUsers } from "react-icons/fa";
 import { AiOutlineLineChart } from "react-icons/ai";
 import { BiImageAdd } from "react-icons/bi";
+import useAuth from "../../../customHooks/useAuth";
 
 const NavPanel = () => {
-  const dispatch = useDispatch();
   const session = useSelector((state) => state.auth);
+  const { logout } = useAuth();
 
-  const handleLogout = () => {
-    window.localStorage.removeItem("user");
-    dispatch(setLogout());
-  };
   return (
     <>
       <div className="admin-bar">
         <div className="user">
           <Link to="/admin">
-            <img src={img} className="img-profile" alt="profile"/>
+            <img src={img} className="img-profile" alt="profile" />
             <h5>{window.localStorage.getItem("user")}</h5>
           </Link>
           <Link to="/admin"> My Profile</Link>
         </div>
         <ul className="menu-options">
           <li>
-            <AiOutlineLineChart className="icon"/> SALES
+            <AiOutlineLineChart className="icon" /> SALES
           </li>
           <li>
-            <FaUsers className="icon"/> USERS
+            <FaUsers className="icon" /> USERS
           </li>
           <li>
             <Link to="/admin/addartist">
-              <BsPersonBadge className="icon"/> ARTISTS
+              <BsPersonBadge className="icon" /> ARTISTS
             </Link>
           </li>
           <li>
             <Link to="/admin/additems">
               {" "}
-              <BiImageAdd className="icon"/> ITEMS
+              <BiImageAdd className="icon" /> ITEMS
             </Link>
           </li>
           {session && (
-            <li onClick={() => handleLogout()}>
+            <li onClick={() => logout()}>
               Logout
-              <FiLogOut className="icon"/>
+              <FiLogOut className="icon" />
             </li>
           )}
         </ul>
