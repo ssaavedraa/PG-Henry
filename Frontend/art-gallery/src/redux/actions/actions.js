13f404eb1783bd4671be81b0ad6b569189d70449
unknown <nunezsantiago@gmail.com>
2022-03-10 19:02:08 -0300

    switched from fetch to axios.get

--- Frontend/art-gallery/src/redux/actions/actions.js
+++ Frontend/art-gallery/src/redux/actions/actions.js
@@ -10,7 +10,7 @@ import {
   GET_TECHNIQUE,
   GET_SEARCH,
   POST_FAVS,
-  DELETE_FAVS
+  DELETE_FAVS,
 } from "../action-types/index.js";
 
 export function getPaintings(filters) {
@@ -92,8 +92,9 @@ export function getPaitingsByArtist(id) {
 export const getObraDetail = (id) => {
   return async (dispatch) => {
     try {
-      let resp = await fetch(`http://localhost:3001/painting/get/${id}`);
-      let data = await resp.json();
+      let { data } = await axios.get(
+        `http://localhost:3001/painting/get/${id}`
+      );
       dispatch({
         type: GET_OBRAID,
         payload: data,
@@ -106,10 +107,9 @@ export const getObraDetail = (id) => {
 export const getObrasRandon = (id) => {
   return async (dispatch) => {
     try {
-      let resp = await fetch(
+      let { data } = await axios.get(
         `http://localhost:3001/painting/getrecommended/${id}`
       );
-      let data = await resp.json();
       dispatch({
         type: GET_OBRAIDRANDON,
         payload: data,
@@ -193,13 +193,13 @@ export function getSearchAuto(text) {
 
 //Post favs
 export function postFav(id) {
-  console.log('soy pintura n|', id)
+  console.log("soy pintura n|", id);
   return async function (dispatch) {
     try {
       const json = await axios.post(
-      `http://localhost:3001/favorites/add/${id}`
+        `http://localhost:3001/favorites/add/${id}`
       );
-      console.log('soy favs', json)
+      console.log("soy favs", json);
       return dispatch({
         type: POST_FAVS,
         payload: json.data,
@@ -212,13 +212,13 @@ export function postFav(id) {
 
 //Post favs
 export function deleteFav(id) {
-  console.log('soy delete de pintura n|', id)
+  console.log("soy delete de pintura n|", id);
   return async function (dispatch) {
     try {
       const json = await axios.delete(
-      `http://localhost:3001/favorites/remove/${id}`
+        `http://localhost:3001/favorites/remove/${id}`
       );
-      console.log('soy favs', json)
+      console.log("soy favs", json);
       return dispatch({
         type: DELETE_FAVS,
         payload: json.data,
