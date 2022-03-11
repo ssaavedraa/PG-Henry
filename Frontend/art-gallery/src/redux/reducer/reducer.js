9457d55e6dbf9d495573ca182cf6a7f33b7f30ad
brianweidl <brianweidl@hotmail.com>
2022-03-11 15:18:37 -0300

    fav fix front

--- Frontend/art-gallery/src/redux/reducer/reducer.js
+++ Frontend/art-gallery/src/redux/reducer/reducer.js
@@ -1,95 +1,86 @@
 import {
-  GET_OBRAID,
-  GET_OBRAIDRANDON,
-  GET_PAITINGS,
-  GET_PAINTINGS_BY_ARTIST,
-  GET_REVIEWS,
-  GET_ARTIST_ID,
-  GET_ARTIST,
-  GET_TECHNIQUE,
-  GET_SEARCH,
-  POST_FAVS,
-  DELETE_FAVS,
-  GET_FAVS,
+	GET_OBRAID,
+	GET_OBRAIDRANDON,
+	GET_PAITINGS,
+	GET_PAINTINGS_BY_ARTIST,
+	GET_REVIEWS,
+	GET_ARTIST_ID,
+	GET_ARTIST,
+	GET_TECHNIQUE,
+	GET_SEARCH,
+	POST_FAVS,
+	DELETE_FAVS,
+	GET_FAVS,
 } from "../action-types";
 
 const initialState = {
-  //Aca estan todas las pinturas
-  paintings: [],
-  reviews: [],
-  paintingsArtist: [],
-  artistId: [],
-  artist: [],
-  technique: [],
-  resultSearch: [],
-  favs: [],
+	//Aca estan todas las pinturas
+	paintings: [],
+	reviews: [],
+	paintingsArtist: [],
+	artistId: [],
+	artist: [],
+	technique: [],
+	resultSearch: [],
+	favs: [],
 };
 
 function rootReducer(state = initialState, action) {
-  switch (action.type) {
-    case GET_PAITINGS:
-      return {
-        ...state,
-        paintings: action.payload,
-      };
-    case GET_PAINTINGS_BY_ARTIST:
-      return {
-        ...state,
-        paintingsArtist: action.payload,
-      };
-    case GET_ARTIST_ID:
-      return {
-        ...state,
-        artistId: action.payload,
-      };
-    case GET_REVIEWS:
-      return {
-        ...state,
-        reviews: action.payload,
-      };
-    case GET_ARTIST:
-      return {
-        ...state,
-        artist: action.payload,
-      };
-    case GET_TECHNIQUE:
-      return {
-        ...state,
-        technique: action.payload,
-      };
-    case GET_OBRAID:
-      return {
-        ...state,
-        detailObra: action.payload,
-      };
-    case GET_OBRAIDRANDON:
-      return {
-        ...state,
-        obraRandon: action.payload,
-      };
-    case GET_SEARCH:
-      return {
-        ...state,
-        resultSearch: action.payload,
-      };
-    case POST_FAVS:
-      return {
-        ...state,
-        favs: action.payload,
-      };
-    case DELETE_FAVS:
-      return {
-        ...state,
-        favs: action.payload,
-      };
-    case GET_FAVS:
-      return {
-        ...state,
-        favs: action.payload,
-      };
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case GET_PAITINGS:
+			return {
+				...state,
+				paintings: action.payload,
+			};
+		case GET_PAINTINGS_BY_ARTIST:
+			return {
+				...state,
+				paintingsArtist: action.payload,
+			};
+		case GET_ARTIST_ID:
+			return {
+				...state,
+				artistId: action.payload,
+			};
+		case GET_REVIEWS:
+			return {
+				...state,
+				reviews: action.payload,
+			};
+		case GET_ARTIST:
+			return {
+				...state,
+				artist: action.payload,
+			};
+		case GET_TECHNIQUE:
+			return {
+				...state,
+				technique: action.payload,
+			};
+		case GET_OBRAID:
+			return {
+				...state,
+				detailObra: action.payload,
+			};
+		case GET_OBRAIDRANDON:
+			return {
+				...state,
+				obraRandon: action.payload,
+			};
+		case GET_SEARCH:
+			return {
+				...state,
+				resultSearch: action.payload,
+			};
+
+		case GET_FAVS:
+			return {
+				...state,
+				favs: action.payload,
+			};
+		default:
+			return state;
+	}
 }
 
 export default rootReducer;
