02231087033431bf1325ad075765036556c71b8f
Santiago Saavedra <66895659+ssaavedraa@users.noreply.github.com>
2022-03-12 07:42:42 -0500

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,7 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 
 
-const { LOAD_MOCK_DATA } = process.env.LOAD_MOCK_DATA || true;
+const { MOCK_DATA } = process.env.LOAD_MOCK_DATA || true;
 const port = process.env.PORT || 3001
 // Syncing all the models at once.
 
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3001
 conn
   .sync({ force: true })
   .then(() => {
-    if (LOAD_MOCK_DATA) loadMockData();
+    if (MOCK_DATA) loadMockData();
   })
   .then(() => {
     conn.query("CREATE EXTENSION IF NOT EXISTS  pg_trgm;");
