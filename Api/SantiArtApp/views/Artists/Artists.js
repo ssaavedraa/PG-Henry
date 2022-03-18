59b4d86d6239a5ced9ce960f83e93a917139b810
unknown <nunezsantiago@gmail.com>
2022-03-18 02:55:05 -0300

    artists GET example

--- Api/SantiArtApp/views/Artists/Artists.js
+++ Api/SantiArtApp/views/Artists/Artists.js
@@ -1,7 +1,50 @@
-import { Text } from "react-native";
+import { useEffect, useState } from "react";
+import { ActivityIndicator, Image, Text, View } from "react-native";
+import getArtists from "../../selectors/getArtists";
 
 function Artists() {
-  return <Text>Artists</Text>;
+  const [artistData, setArtistData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    getArtists()
+      .then((artists) => {
+        setArtistData(artists);
+        console.log("LOADED ARTISTS");
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading)
+    return (
+      <ActivityIndicator
+        style={{
+          flex: 1,
+          justifyContent: "center",
+        }}
+        size="large"
+        color="#0000ff"
+      />
+    );
+  return (
+    <View>
+      {artistData.map(({ name }, i) => (
+        <Text key={i}>{name}</Text>
+      ))}
+    </View>
+  );
 }
 
 export default Artists;
+/* <Image
+          key={i}
+          style={{ width: 50, height: 50 }}
+          source={{
+            uri: { photo },
+          }}
+        /> */
