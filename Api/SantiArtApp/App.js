e836c4ee81a7291a15f55dab34bb2f748b99cd4e
unknown <nunezsantiago@gmail.com>
2022-03-18 00:54:29 -0300

    first api commit

---
+++ Api/SantiArtApp/App.js
@@ -0,0 +1,29 @@
+import "react-native-gesture-handler";
+import * as React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import Login from "./views/Login/Login";
+import Home from "./views/Home";
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ Api/SantiArtApp/views/Artists/Artists.js
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+function Artists() {
+  return <Text>Artists</Text>;
+}
+
+export default Artists;
