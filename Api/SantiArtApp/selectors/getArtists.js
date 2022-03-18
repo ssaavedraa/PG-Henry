a1c37038f8a052bee7689408d56b5073e3db0120
unknown <nunezsantiago@gmail.com>
2022-03-18 02:55:05 -0300

    artists GET example

---
+++ Api/SantiArtApp/selectors/getArtists.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const getArtists = async () => {
+  try {
+    const { data } = await axios.get("/artist/getstats");
+    return data;
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
+
+export default getArtists;
