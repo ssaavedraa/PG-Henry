bf9596c35ec51788bf5830feb8c764130ad7e90a
Eliana Alvarez <alvarez.eliana00@gmail.com>
2022-03-18 23:57:13 -0300

    algunos estilos al login

--- Api/SantiArtApp/views/Login/Login.style.js
+++ Api/SantiArtApp/views/Login/Login.style.js
@@ -1,4 +1,9 @@
 import { StyleSheet } from "react-native";
+//Es para las pantallas responsives
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 
 export default StyleSheet.create({
   loginForm: {
@@ -6,5 +11,31 @@ export default StyleSheet.create({
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
+    backgroundColor: 'white'
   },
+  inputs: {
+    width: wp('90%'),
+    height: hp('8%'),
+    borderColor: '#F3F1F5',
+    borderWidth: wp('0.9%'),
+    marginTop: wp('7%'),
+    borderRadius: wp('4%'),
+    fontSize: hp('2.15%'),
+    paddingLeft: wp('2.5%')
+  },
+  btnLogin: {
+    width: wp('90%'),
+    height: hp('8%'),
+    backgroundColor: '#168AAD',
+    marginTop: wp('7%'),
+    borderRadius: wp('4%')
+  },
+  textLogin: {
+    color: 'white',
+    fontSize: hp('3%'),
+    textAlign: 'center',
+    marginTop: wp('2.75%'),
+    fontWeight: "bold"
+    
+  }
 });
