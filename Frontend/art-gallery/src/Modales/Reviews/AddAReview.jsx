c0cabb6939181eeff87e7a676a60f36877931ec9
yessicarm <89185291+yessicarm@users.noreply.github.com>
2022-03-20 07:42:52 -0700

    review modal listo

--- Frontend/art-gallery/src/Modales/Reviews/AddAReview.jsx
+++ Frontend/art-gallery/src/Modales/Reviews/AddAReview.jsx
@@ -8,22 +8,27 @@ import { useNavigate } from "react-router-dom";
 
 import { FaStar } from "react-icons/fa";
 
-const AddAReview= (setOpenModalArtist) => {
+const AddAReview = (setOpenModalArtist) => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const [input, setInput] = useState({
-    score: 0,
-    review: "",    
-  });
 
   const [errors, setError] = useState({});
   const [applyChanges, setApplyChanges] = useState(true);
 
+  const [hover, SetHover] = useState(null);
+
+  const [input, setInput] = useState({
+    score: 0,
+    review: "",
+  });
+
+  console.log(input, "soy input");
+
   function validate(input) {
     setApplyChanges(true);
     let errors = {};
-    if (!input.score|| !input.review) {
-      errors.message = "*Please fill all the inputs";    
+    if (!input.score || !input.review) {
+      errors.message = "*Please fill all the inputs";
     } else {
       setApplyChanges(false);
     }
@@ -31,10 +36,9 @@ const AddAReview= (setOpenModalArtist) => {
   }
 
   function handleChange(e) {
-
     setInput({
       ...input,
-      [e.target.name]: e.target.value, 
+      [e.target.name]: e.target.value,
     });
     setError(
       validate({
@@ -60,17 +64,18 @@ const AddAReview= (setOpenModalArtist) => {
 
   return (
     <>
-      <div className="artists-box">
-        <div className="img-container"></div>
-        <div className="info-box">
+      <div className="review-box">
+        <div className="img-container2"></div>
+        <div className="inforeview-box">
           <div className="profile-logo">
             <img src={logo} height="70rem" alt="imgUser" />
           </div>
-          <div><h1>Review your purchase</h1></div>
-          <div className="data-box">
-            <form key="form" onSubmit={(e) => handleSubmit(e)}>                  
 
-             
+          <div className="data-box-review">
+            <form key="form" onSubmit={(e) => handleSubmit(e)}>
+              <div>
+                <h2>Review your purchase</h2>
+              </div>
 
               <label> Product review: </label>
               <textarea
@@ -80,25 +85,38 @@ const AddAReview= (setOpenModalArtist) => {
                 value={input.review}
                 onChange={handleChange}
               />
+              <label> What is your score for the artist?</label>
+              <div className="stars">
+                {[...Array(5)].map((star, i) => {
+                  const score = i + 1;
+                  return (
+                    <label>
+                      <input
+                        type="radio"
+                        name="score"
+                        value={input.score}
+                        onClick={() => setInput({ score })}
+                      />
+                      <FaStar
+                        className="star"
+                        size="2rem"
+                        color={
+                          score <= (hover || input.score) ? "#ffc107" : "e4e5e9"
+                        }
+                        onMouseEnter={() => SetHover(score)}
+                        onMouseLeave={() => SetHover(null)}
+                      />
+                    </label>
+                  );
+                })}
+              </div>
 
-               <label> What is your score for the artist?</label>
-              
-              <input
-                type="text"
-                autoComplete="off"
-                key="email"
-                className="input-add-review"
-                required
-                value={input.score}
-                name="score"
-                onChange={handleChange}
-              />
               <div>
                 <div className="error">
-                  {errors.message ? <p>{errors.message}</p> : <p></p>}{" "}
+                  {errors.message ? <p>{errors.message}</p> : <p></p>}
                 </div>
                 <button disabled={applyChanges} className="btn-addartist">
-                SEND MY REVIEW
+                  SEND MY REVIEW
                 </button>
               </div>
             </form>
