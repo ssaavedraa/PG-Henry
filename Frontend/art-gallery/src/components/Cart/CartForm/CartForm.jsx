5b739b26a388e67ee355a7418f7a90a06af3c4c2
brianweidl <brianweidl@hotmail.com>
2022-03-20 19:36:01 -0300

    add purchase details

--- Frontend/art-gallery/src/components/Cart/CartForm/CartForm.jsx
+++ Frontend/art-gallery/src/components/Cart/CartForm/CartForm.jsx
@@ -2,135 +2,143 @@ import React, { useEffect, useState } from "react";
 import "./CartForm.css";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import useCart from "../../../customHooks/useCart.js"
+import useCart from "../../../customHooks/useCart.js";
+import { contactInfo } from "../../../redux/actions/actions";
+import { useDispatch } from "react-redux";
 
 const CartForm = () => {
-  //Esto es lo que estaba en contactInfo
-  const navigate = useNavigate();
-  const { cart } = useCart();
+	//Esto es lo que estaba en contactInfo
+	const navigate = useNavigate();
+	const { cart } = useCart();
+	const dispatch = useDispatch();
 
-  const getPaintings = async () => {
-    const paintings = [];
-    for (let i = 0; i < cart.length; i++) {
-      const dbPaiting = await axios.get(`/painting/get/${cart[i]}`);
-      paintings.push(dbPaiting.data);
-    }
-    return paintings;
-  };
+	const getPaintings = async () => {
+		const paintings = [];
+		for (let i = 0; i < cart.length; i++) {
+			const dbPaiting = await axios.get(`/painting/get/${cart[i]}`);
+			paintings.push(dbPaiting.data);
+		}
+		return paintings;
+	};
 
-  const [paintings, setPaintings] = useState([]);
-  console.log(paintings)
-  const [info, setInfo] = useState({
-      firstName: "",
-      email: "", 
-      lastName: "",
-      telephone: null,
-      postCode: null,
-      city: "",
-      street: "",
-      streetNumber: null,
-      floor: null,
-      unit: "",
-      paintings: cart,
-      purchaseId: localStorage.getItem("purchaseId")
-  });
+	const [paintings, setPaintings] = useState([]);
 
-  useEffect(() => {
-    setInfo({ ...info, paintings: cart });
-    getPaintings().then(res => setPaintings(res)).catch(err => console.log(err));
-  }, [cart]);
+	const [info, setInfo] = useState({
+		firstName: "",
+		email: "",
+		lastName: "",
+		telephone: null,
+		postCode: null,
+		city: "",
+		street: "",
+		streetNumber: null,
+		floor: null,
+		unit: "",
+		paintings: cart,
+		purchaseId: localStorage.getItem("purchaseId"),
+	});
 
-  const handleChange = (e) => {
-    const value = e.target.value;
-    const type = e.target.name;
-    setInfo({ ...info, [type]: value });
-  };
+	useEffect(() => {
+		setInfo({ ...info, paintings: cart });
+		getPaintings()
+			.then((res) => setPaintings(res))
+			.catch((err) => console.log(err));
+	}, [cart]);
+
+	const handleChange = (e) => {
+		const value = e.target.value;
+		const type = e.target.name;
+		setInfo({ ...info, [type]: value });
+	};
 
 	const handleSubmit = async (e) => {
-    e.preventDefault();
-    for (const key in info) {
-      if (!info[key] && key !== "floor" && key !== "unit") {
-        return
-      }
-    };
+		e.preventDefault();
+		/* for (const key in info) {
+			if (!info[key] && key !== "floor" && key !== "unit") {
+				return;
+			}
+		} */
 		try {
 			await axios.post("checkout/contactInfo", info);
-      
+			console.log(contactInfo);
+			console.log(info);
+			dispatch(contactInfo(info));
 			navigate("/payment");
 		} catch (e) {
 			console.log(e);
 		}
 	};
 
-  return (
-    <div className="divContainerFormCart">
-      <h1>Purchasing process</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="divContainerDetailsCart">
-          <div className="divContainerInputsFormCart">
-            <h3> Billings details</h3>
-            <div className="divContainerNameLastCart">
-              <div className="divNameLastCart">
-                <label>First Name</label>
-                <input type="text" name="firstName" onChange={handleChange}/>
-              </div>
-              <div className="divNameLastCart">
-                <label>Last Name</label>
-                <input type="text" name="lastName" onChange={handleChange}/>
-              </div>
-            </div>
+	return (
+		<div className="divContainerFormCart">
+			<h1>Purchasing process</h1>
+			<form onSubmit={handleSubmit}>
+				<div className="divContainerDetailsCart">
+					<div className="divContainerInputsFormCart">
+						<h3> Billings details</h3>
+						<div className="divContainerNameLastCart">
+							<div className="divNameLastCart">
+								<label>First Name</label>
+								<input type="text" name="firstName" onChange={handleChange} />
+							</div>
+							<div className="divNameLastCart">
+								<label>Last Name</label>
+								<input type="text" name="lastName" onChange={handleChange} />
+							</div>
+						</div>
 
-            <label>Email</label>
-            <input type="email" name="email" onChange={handleChange}/>
+						<label>Email</label>
+						<input type="email" name="email" onChange={handleChange} />
 
-            <label>Telephone</label>
-            <input type="text" name="telephone" onChange={handleChange}/>
+						<label>Telephone</label>
+						<input type="text" name="telephone" onChange={handleChange} />
 
-            <label>Post Code</label>
-            <input type="text" name="postCode" onChange={handleChange}/>
+						<label>Post Code</label>
+						<input type="text" name="postCode" onChange={handleChange} />
 
-            <label>City</label>
-            <input type="text" name="city" onChange={handleChange}/>
+						<label>City</label>
+						<input type="text" name="city" onChange={handleChange} />
 
-            <label>Street</label>
-            <input type="text" name="street" onChange={handleChange}/>
-            <div className="divContainerNumberFloorUnit">
-              <div className="NumberFloorUnitCart">
-                <label>Number</label>
-                <input type="text" name="streetNumber" onChange={handleChange}/>
-              </div>
+						<label>Street</label>
+						<input type="text" name="street" onChange={handleChange} />
+						<div className="divContainerNumberFloorUnit">
+							<div className="NumberFloorUnitCart">
+								<label>Number</label>
+								<input type="text" name="streetNumber" onChange={handleChange} />
+							</div>
 
-              <div className="NumberFloorUnitCart">
-                <label>Floor</label>
-                <input type="text" name="floor" onChange={handleChange}/>
-              </div>
+							<div className="NumberFloorUnitCart">
+								<label>Floor</label>
+								<input type="text" name="floor" onChange={handleChange} />
+							</div>
 
-              <div className="NumberFloorUnitCart">
-                <label>Unit</label>
-                <input type="text" name="unit" onChange={handleChange}/>
-              </div>
-            </div>
-          </div>
+							<div className="NumberFloorUnitCart">
+								<label>Unit</label>
+								<input type="text" name="unit" onChange={handleChange} />
+							</div>
+						</div>
+					</div>
 
-          <div className="divContainerItemsCart">
-            <h3>Your order</h3>
-              {paintings && paintings.map((painting, i) => (
-                <div key={i} className="divContainerProductCart">
-                  <div>
-                    <p>{painting.title}</p>
-                    <img src={painting.photos[0].url} alt={painting.title} />
-                  </div>
-                  <p>USD$ {painting.price}</p>
-                </div>
-              ))}
-            <div className="divContainerButtonCartContinue">
-              {<button type="submit">Continue</button>}
-            </div>
-          </div>
-        </div>
-      </form>
-    </div>
-  );
+					<div className="divContainerItemsCart">
+						<h3>Your order</h3>
+						{paintings &&
+							paintings.map((painting, i) => (
+								<div key={i} className="divContainerProductCart">
+									<div>
+										<p>{painting.title}</p>
+										<img src={painting.photos[0].url} alt={painting.title} />
+									</div>
+									<p>USD$ {painting.price}</p>
+								</div>
+							))}
+						<div className="divContainerButtonCartContinue">
+							{<button type="submit">Continue</button>}
+						</div>
+					</div>
+				</div>
+			</form>
+		</div>
+	);
 };
 
 export default CartForm;
--- Frontend/art-gallery/src/redux/reducer/reducer.js
+++ Frontend/art-gallery/src/redux/reducer/reducer.js
@@ -1,204 +1,213 @@
 import {
-  GET_OBRAID,
-  GET_OBRAIDRANDON,
-  GET_PAITINGS,
-  GET_PAINTINGS_BY_ARTIST,
-  GET_REVIEWS,
-  GET_ARTIST_ID,
-  GET_ARTIST,
-  GET_TECHNIQUE,
-  GET_SEARCH,
-  POST_FAVS,
-  DELETE_FAVS,
-  GET_USER_ADMIN,
-  ORDER_BY_A_Z,
-  ORDER_BY_TYPE,
-  GET_FAVS,
-  CLEAR_ARTISTBYID,
+	GET_OBRAID,
+	GET_OBRAIDRANDON,
+	GET_PAITINGS,
+	GET_PAINTINGS_BY_ARTIST,
+	GET_REVIEWS,
+	GET_ARTIST_ID,
+	GET_ARTIST,
+	GET_TECHNIQUE,
+	GET_SEARCH,
+	POST_FAVS,
+	DELETE_FAVS,
+	GET_USER_ADMIN,
+	ORDER_BY_A_Z,
+	ORDER_BY_TYPE,
+	GET_FAVS,
+	CLEAR_ARTISTBYID,
 	GET_STATS_ARTIST,
-  ADD_ARTIST,
-  EDIT_PAINT,
-  EDIT_ARTIST,
-  ADD_TECHNIQUE,
-  ADD_PAINTING
+	ADD_ARTIST,
+	EDIT_PAINT,
+	EDIT_ARTIST,
+	ADD_TECHNIQUE,
+	ADD_PAINTING,
+	CONTACT_INFO,
 } from "../action-types";
 
-
 const detailObra = {
-  orientation: "",
-  id: 1,
-  title: "",
-  description: "",
-  height: 0,
-  width: 0,
-  price: 0,
-  techniques: [
-    {
-      id: 1,
-      name: "",
-      description: "",
-    },
-  ],
-  artist: {
-    id: 1,
-    name: "",
-    biography: "",
-    photo: "",
-    email: "",
-    score: 1,
-  },
-  photos: [
-    {
-      url: "",
-    },
-  ],
-}
+	orientation: "",
+	id: 1,
+	title: "",
+	description: "",
+	height: 0,
+	width: 0,
+	price: 0,
+	techniques: [
+		{
+			id: 1,
+			name: "",
+			description: "",
+		},
+	],
+	artist: {
+		id: 1,
+		name: "",
+		biography: "",
+		photo: "",
+		email: "",
+		score: 1,
+	},
+	photos: [
+		{
+			url: "",
+		},
+	],
+};
 
 const artistId = {
-    id: 1,
-    name: "",
-    biography: "",
-    photo: "",
-    email: "",
-    location: "",
-}
+	id: 1,
+	name: "",
+	biography: "",
+	photo: "",
+	email: "",
+	location: "",
+};
 
 const initialState = {
-  //Aca estan todas las pinturas
-  paintings: [],
-  reviews: [],
-  paintingsArtist: [],
-  artistId,
-  artist: [],
-  technique: [],
-  resultSearch: [],
-  favs: [],
-  detailObra,
-
+	//Aca estan todas las pinturas
+	paintings: [],
+	reviews: [],
+	paintingsArtist: [],
+	artistId,
+	artist: [],
+	technique: [],
+	resultSearch: [],
+	favs: [],
+	detailObra,
+	contactInfo: {},
 };
 
 function rootReducer(state = initialState, action) {
-  switch (action.type) {
-    case GET_PAITINGS:
-      return {
-        ...state,
-        paintings: action.payload,
-      };
-    case GET_PAINTINGS_BY_ARTIST:
-      return {
-        ...state,
-        paintingsArtist: action.payload,
-      };
-    case GET_ARTIST_ID:
-      return {
-        ...state,
-        artistId: action.payload,
-      };
-    case GET_REVIEWS:
-      return {
-        ...state,
-        reviews: action.payload,
-      };
-    case GET_ARTIST:
-      return {
-        ...state,
-        artist: action.payload,
-      };
-    case GET_TECHNIQUE:
-      return {
-        ...state,
-        technique: action.payload,
-      };
-    case GET_OBRAID:
-      return {
-        ...state,
-        detailObra: action.payload,
-      };
-    case GET_OBRAIDRANDON:
-      return {
-        ...state,
-        obraRandon: action.payload,
-      };
-    case GET_SEARCH:
-      return {
-        ...state,
-        resultSearch: action.payload,
-      };
-    case POST_FAVS:
-      return {
-        ...state,
-        favs: action.payload,
-      };
-    case DELETE_FAVS:
-      return {
-        ...state,
-        favs: action.payload,
-      };
-    case GET_USER_ADMIN:
-      return {
-        ...state,
-        userAdmin: action.payload,
-      };
-    case ORDER_BY_A_Z:
-      return {
-        ...state,
-        userAdmin: action.payload,
-      };
+	switch (action.type) {
+		case GET_PAITINGS:
+			return {
+				...state,
+				paintings: action.payload,
+			};
+		case GET_PAINTINGS_BY_ARTIST:
+			return {
+				...state,
+				paintingsArtist: action.payload,
+			};
+		case GET_ARTIST_ID:
+			return {
+				...state,
+				artistId: action.payload,
+			};
+		case GET_REVIEWS:
+			return {
+				...state,
+				reviews: action.payload,
+			};
+		case GET_ARTIST:
+			return {
+				...state,
+				artist: action.payload,
+			};
+		case GET_TECHNIQUE:
+			return {
+				...state,
+				technique: action.payload,
+			};
+		case GET_OBRAID:
+			return {
+				...state,
+				detailObra: action.payload,
+			};
+		case GET_OBRAIDRANDON:
+			return {
+				...state,
+				obraRandon: action.payload,
+			};
+		case GET_SEARCH:
+			return {
+				...state,
+				resultSearch: action.payload,
+			};
+		case POST_FAVS:
+			return {
+				...state,
+				favs: action.payload,
+			};
+		case DELETE_FAVS:
+			return {
+				...state,
+				favs: action.payload,
+			};
+		case GET_USER_ADMIN:
+			return {
+				...state,
+				userAdmin: action.payload,
+			};
+		case ORDER_BY_A_Z:
+			return {
+				...state,
+				userAdmin: action.payload,
+			};
 
-    case ORDER_BY_TYPE:
-      return {
-        ...state,
-        userAdmin: action.payload
-      }
-      case GET_FAVS:
+		case ORDER_BY_TYPE:
+			return {
+				...state,
+				userAdmin: action.payload,
+			};
+		case GET_FAVS:
 			return {
 				...state,
 				favs: action.payload,
 			};
 
 		case CLEAR_ARTISTBYID:
-			return{
+			return {
 				...state,
 				artistId: [],
-			}
+			};
 
 		case GET_STATS_ARTIST:
-			return{
+			return {
 				...state,
-				artist: action.payload
-			}
+				artist: action.payload,
+			};
 
-    case ADD_ARTIST:
-      return{
-        ...state,
-        artist: [...state.artist, action.payload]
-      }
-      
-      case ADD_PAINTING:
-      return{
-        ...state,
-        paintings: [...state.paintings, action.payload]
-      }
-    case EDIT_PAINT:      
-      let artworks = [...state.paintings]
-      let modifiedArtworkIndex = artworks.findIndex(paint => paint.id === action.payload[0].id)
-      artworks[modifiedArtworkIndex] = action.payload[0]
-      return{
-        ...state,
-        paintings: artworks
-      }
-      case ADD_TECHNIQUE:
-      return{
-        ...state,
-        technique: [...state.technique, action.payload]
-      }
-      case EDIT_ARTIST:
-      return{
-        ...state,
-        artistId: [...state.artistId, action.payload]
-      }
-      
-    default:
+		case ADD_ARTIST:
+			return {
+				...state,
+				artist: [...state.artist, action.payload],
+			};
+
+		case ADD_PAINTING:
+			return {
+				...state,
+				paintings: [...state.paintings, action.payload],
+			};
+		case EDIT_PAINT:
+			let artworks = [...state.paintings];
+			let modifiedArtworkIndex = artworks.findIndex(
+				(paint) => paint.id === action.payload[0].id
+			);
+			artworks[modifiedArtworkIndex] = action.payload[0];
+			return {
+				...state,
+				paintings: artworks,
+			};
+		case ADD_TECHNIQUE:
+			return {
+				...state,
+				technique: [...state.technique, action.payload],
+			};
+		case EDIT_ARTIST:
+			return {
+				...state,
+				artistId: [...state.artistId, action.payload],
+			};
+		case CONTACT_INFO:
+			console.log("a");
+			console.log(action.payload);
+			return {
+				...state,
+				contactInfo: action.payload,
+			};
+
+		default:
 			return state;
 	}
 }
