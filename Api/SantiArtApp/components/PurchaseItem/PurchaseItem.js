57e56ab139e518d72224ea5120ce7dee9385d342
Eliana Alvarez <alvarez.eliana00@gmail.com>
2022-03-21 19:36:15 -0300

    touchable opacity en purchases, saca el icono

--- Api/SantiArtApp/components/PurchaseItem/PurchaseItem.js
+++ Api/SantiArtApp/components/PurchaseItem/PurchaseItem.js
@@ -15,34 +15,30 @@ const stateColors = {
 function PurchaseItem({ purchase, navigation }) {
   return (
     <ListItem>
-      <View style={style.containerPurchase}>
-        <View>
-          <Text style={style.textName}>
-            {purchase.contactInfo.firstName} {purchase.contactInfo.lastName}
-          </Text>
-          <Text
-            style={[
-              style.textPurchase,
-              {
-                color: stateColors.hasOwnProperty(purchase.state)
-                  ? stateColors[purchase.state]
-                  : "grey",
-              },
-            ]}
-          >
-            {purchase.state}
-          </Text>
+      <TouchableOpacity
+        onPress={() => navigation.navigate("PurchaseDetail", { purchase })}
+      >
+        <View style={style.containerPurchase}>
+          <View>
+            <Text style={style.textName}>
+              {purchase.contactInfo.firstName} {purchase.contactInfo.lastName}
+            </Text>
+            <Text
+              style={[
+                style.textPurchase,
+                {
+                  color: stateColors.hasOwnProperty(purchase.state)
+                    ? stateColors[purchase.state]
+                    : "grey",
+                },
+              ]}
+            >
+              {purchase.state}
+            </Text>
+          </View>
+          <Text style={style.textName}>$ {purchase.totalPrice}</Text>
         </View>
-        <Text style={style.textName}>$ {purchase.totalPrice}</Text>
-        <TouchableOpacity
-          onPress={() => navigation.navigate("PurchaseDetail", { purchase })}
-        >
-          <MaterialCommunityIcons
-            style={{ fontSize: 30 }}
-            name="ellipsis-vertical"
-          />
-        </TouchableOpacity>
-      </View>
+      </TouchableOpacity>
     </ListItem>
   );
 }
