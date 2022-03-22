8c551e7c22b3215dfc30b518c4be77eef2fc55e4
brianweidl <brianweidl@hotmail.com>
2022-03-21 21:06:37 -0300

    add reviewed attr to getAll paintings

--- Backend/src/controllers/painting/utils/getPaintings.js
+++ Backend/src/controllers/painting/utils/getPaintings.js
@@ -1,38 +1,52 @@
-const { Painting, Artist, Technique, Photo } = require("../../../db");
+const { Painting, Artist, Technique, Photo, Review } = require("../../../db");
 
 const getPaintings = async (condition, techniqueCondition) => {
-  const paintings = await Painting.findAll({
-    ...condition,
-    attributes: [
-      "id",
-      "title",
-      "description",
-      "orientation",
-      "height",
-      "width",
-      "price",
-      "isAvailable",
-    ],
-    include: [
-      {
-        ...techniqueCondition,
-        model: Technique,
-        through: {
-          attributes: [],
-        },
-        attributes: ["id", "name", "description"],
-      },
-      {
-        model: Artist,
-        attributes: ["id", "name", "biography", "photo", "email", "score"],
-      },
-      {
-        model: Photo,
-        attributes: ["url"],
-      },
-    ],
-  });
-  return paintings;
+	let paintings = await Painting.findAll({
+		...condition,
+		attributes: [
+			"id",
+			"title",
+			"description",
+			"orientation",
+			"height",
+			"width",
+			"price",
+			"isAvailable",
+		],
+		include: [
+			{
+				...techniqueCondition,
+				model: Technique,
+				through: {
+					attributes: [],
+				},
+				attributes: ["id", "name", "description"],
+			},
+			{
+				model: Artist,
+				attributes: ["id", "name", "biography", "photo", "email", "score"],
+			},
+			{
+				model: Photo,
+				attributes: ["url"],
+			},
+			{
+				model: Review,
+			},
+		],
+	});
+	paintings = paintings.map((p) => {
+		p = p.toJSON();
+		if (p.reviews.length) {
+			p.isReviewed = true;
+		} else {
+			p.isReviewed = false;
+		}
+
+		return p;
+	});
+
+	return paintings;
 };
 
 module.exports = getPaintings;
