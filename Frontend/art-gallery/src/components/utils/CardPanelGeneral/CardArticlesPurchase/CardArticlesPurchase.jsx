6b94b556ac823f9bc8589e61557500e9f7320ad7
jorgyrod <jorgsrodriguez@gmail.com>
2022-03-23 15:50:49 -0500

    Se ajusta el tamaño de articulos cuando son varios, se linkean estos articulos

--- Frontend/art-gallery/src/components/utils/CardPanelGeneral/CardArticlesPurchase/CardArticlesPurchase.jsx
+++ Frontend/art-gallery/src/components/utils/CardPanelGeneral/CardArticlesPurchase/CardArticlesPurchase.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import DivPurchaseData from "../../DivPurchaseData/DivPurchaseData";
+import { Link } from "react-router-dom";
 import "./CardArticles.css";
 import { useState } from "react";
 import AddReviewModal from "../../../../Modales/Reviews/AddReviewModal";
@@ -9,7 +10,7 @@ function CardArticlesPurchase({ id, price, img, title, isAdmin, state, isReviewe
   const [isModalOpened, setIsModalOpened] = useState(false)
   const [openModalReview, setOpenModalReview] = useState(false)
   return (
-    <div className="articleCard">
+    <Link to={"/detailpainting/" + id} className="articleCard">
       <section className="sectionSupArticle">
         <DivPurchaseData title="Id" value={id} />
         <DivPurchaseData title="Price" value={price} />
@@ -26,7 +27,7 @@ function CardArticlesPurchase({ id, price, img, title, isAdmin, state, isReviewe
       {
         <AddReviewModal openModalReview={openModalReview} setOpenModalReview ={setOpenModalReview}/>
       }
-      </div>
+      </Link>
   );
 }
 
