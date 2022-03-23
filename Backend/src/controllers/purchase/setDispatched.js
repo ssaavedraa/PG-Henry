f7363ec4ccd268c74debe1f09480af46f2776f43
Facundo-Romano <facuromano121299@gmail.com>
2022-03-23 17:27:36 -0300

    payment cancelled, dispatched and completed mails

--- Backend/src/controllers/purchase/setDispatched.js
+++ Backend/src/controllers/purchase/setDispatched.js
@@ -1,10 +1,19 @@
 const { Purchase } = require("../../db");
 const getToday = require("./Utils/getToday");
+const emailSender = require("../../nodemailer/paymentDispatched/sendMail.js");
 
 const setDispatched = async (req, res) => {
   const { id } = req.params;
   try {
-    const purchase = await Purchase.findByPk(id);
+    const purchase = await Purchase.findOne({ where: {id},
+      include: [
+      {
+      model: User,
+      attributes: ["email", "firstName"] 
+      }, {
+      model: ContactInfo,
+      attributes: ["email", "firstName"] 
+      }]});
     if (!purchase)
       return res.json({ status: "error", message: "Purchase doesn't exist" });
     if (purchase.state !== "pending") {
@@ -16,6 +25,12 @@ const setDispatched = async (req, res) => {
     purchase.state = "dispatched";
     purchase.dispatchedDate = getToday();
     await purchase.save();
+
+    emailSender(purchase.user.email, purchase.user.firstName).catch((err) => console.log(err.message));
+    if (purchase.contactInfo.email !== purchase.user.email) {
+      emailSender(purchase.contactInfo.email, purchase.contactInfo.firstName).catch((err) => console.log(err.message));
+    }
+     
     res.json({
       status: "ok",
       message: "Purchase dispatched",
