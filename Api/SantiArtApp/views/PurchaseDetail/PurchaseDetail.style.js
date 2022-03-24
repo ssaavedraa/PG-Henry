fe8aa344d76f4152aeea9c575d3988671e4a8113
Eliana Alvarez <alvarez.eliana00@gmail.com>
2022-03-24 01:53:34 -0300

    estilos generales en la app

--- Api/SantiArtApp/views/PurchaseDetail/PurchaseDetail.style.js
+++ Api/SantiArtApp/views/PurchaseDetail/PurchaseDetail.style.js
@@ -7,9 +7,51 @@ import {
 
 export default StyleSheet.create({
   container: {
-    backgroundColor: "white",
     padding: wp("3%"),
-    flex: 1,
+  },
+  viewStatus: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingBottom: wp("1%"),
+    margin: wp("0.5%"),
+    borderBottomWidth: wp("0.5%"),
+    borderColor: "#f5f5f5",
+    alignContent: "center",
+    alignItems: "center",
+  },
+  textPrincipal: {
+    fontWeight: "bold",
+    fontSize: hp("2.15%"),
+    // textAlign: 'center' ,
+  },
+  viewPrice: {
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  totalPrice: {
+    fontWeight: "bold",
+    fontSize: hp("2.40%"),
+  },
+  total: {
+    marginRight: wp("3.5%"),
+    fontSize: hp("2%"),
+  },
+  containerStatusDetail: {
+    width: wp("95%"),
+    height: hp("40%"),
+    justifyContent: "space-between",
+  },
+  viewDetailsProducts: {
+    width: wp("42.5%"),
+  },
+  imgItem: {
+    marginTop: wp("2.5%"),
+    marginBottom: wp("2.5%"),
+    marginLeft: wp("2%"),
+    width: wp("36%"),
+    height: hp("14%"),
+    resizeMode: "contain",
   },
   customerInfo: {
     padding: wp("2%"),
@@ -18,54 +60,20 @@ export default StyleSheet.create({
     backgroundColor: "white",
     margin: wp("2%"),
   },
-  Detail: {
-    padding: wp("2%"),
-    borderRadius: wp("2%"),
-    elevation: 4,
-    backgroundColor: "white",
-    margin: wp("2%"),
-    width: wp("90%"),
-    
-  },
-  containerStatusDetail: {
-    width: wp("95%"),
-    height: hp('40%'),
-    justifyContent: "space-between",
-  },
-  viewDetailsProducts: {
-   width: wp('42.5%')
-  },
-  imgItem: {
-    marginTop: wp('2.5%'),
-    marginBottom: wp('2.5%'),
-    marginLeft: wp('2%'),
-    width: wp("36%"),
-    height: hp("16%"),
-    resizeMode: 'contain',
-  },
-  textPrincipal: {
-    fontWeight: 'bold',
-    fontSize: hp('2.25%'),
-    // textAlign: 'center' ,
-    paddingBottom: wp('1%'),
-    margin: wp('0.5%'),
-    borderBottomWidth: wp('0.5%'),
-    borderColor: "#f5f5f5"
-  },
   titlePainting: {
-     fontStyle: 'italic',
-     fontWeight: '900',
-     fontSize: hp('2.10%'),
-     marginTop: wp('1%'),
-     fontWeight: 'bold'
+    fontStyle: "italic",
+    fontWeight: "900",
+    fontSize: hp("2.10%"),
+    marginTop: wp("1%"),
+    fontWeight: "bold",
   },
   textPrice: {
-      fontSize: hp('2.75%'),
-      marginTop: wp('1%'),
-      fontWeight: 'bold'
+    fontSize: hp("2.75%"),
+    marginTop: wp("1%"),
+    fontWeight: "bold",
   },
-  textDetails:{
-    marginTop: wp('0.25%'),
-    fontSize: hp('1.75%')
-  }
+  textDetails: {
+    marginTop: wp("0.25%"),
+    fontSize: hp("1.75%"),
+  },
 });
